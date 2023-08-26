6c1a71d7b46871394f87afb3ceb61da7fa4ab604
Arman <Arman.talebi2000@gmail.com>
2023-08-26 21:34:08 +0300

    Add Icons and Selections of navbar in small devices

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineHome,
+  AiOutlineProject,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
 
 const SideNav = () => {
   const [nav, setNav] = useState(false);
@@ -23,9 +30,37 @@ const SideNav = () => {
             <AiOutlineHome size={20} />
             <span className="pl-2">Home</span>
           </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <GrProjects size={20} />
+            <span className="pl-2">Work</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineProject size={20} />
+            <span className="pl-2">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <BsPerson size={20} />
+            <span className="pl-2">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+          >
+            <AiOutlineMail size={20} />
+            <span className="pl-2">Contact</span>
+          </a>
         </div>
       ) : (
-        <div></div>
+        ""
       )}
     </div>
   );
