0012420b0f10c7207828956cd8e6cfe8d90209fb
Arman <Arman.talebi2000@gmail.com>
2023-08-27 00:54:36 +0300

    Color of nav bars changed

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -22,38 +22,38 @@ const SideNav = () => {
         className="absolute top-4 right-4 z-[99] md:hidden"
       />
       {nav ? (
-        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+        <div className="fixed w-full h-screen bg-white/30 flex flex-col justify-center items-center z-20">
           <a
             href="#main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-sm bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
             <span className="pl-2">Home</span>
           </a>
           <a
             href="#work"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-sm bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-2">Work</span>
           </a>
           <a
             href="#project"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-sm bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
             <span className="pl-2">Projects</span>
           </a>
           <a
             href="#main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-sm bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-2">Resume</span>
           </a>
           <a
             href="#contact"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-300 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-sm bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineMail size={20} />
             <span className="pl-2">Contact</span>
