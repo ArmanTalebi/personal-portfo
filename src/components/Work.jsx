1adcf64dd4531f497519ff82087edce3f276d214
Arman <Arman.talebi2000@gmail.com>
2023-08-30 13:35:37 +0300

    Appearance changes of work items

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,28 +4,28 @@ import WorkItem from "./WorkItem";
 const data = [
   {
     year: 2023,
-    title: "front-end developer (Yanus.ai)",
+    title: " front-end developer (Yanus.ai)",
     duration: "Till now",
     details:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro ullam non quibusdam autem recusandae delectus tempora asperiores, laudantium minima?",
   },
   {
     year: 2023,
-    title: "front-end developer",
+    title: " front-end developer",
     duration: "1 Month",
     details:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro ullam non quibusdam autem recusandae delectus tempora asperiores, laudantium minima?",
   },
   {
     year: 2023,
-    title: "Internship front-end developer & Designer",
+    title: " Internship front-end developer & Designer",
     duration: "6 Months",
     details:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro ullam non quibusdam autem recusandae delectus tempora asperiores, laudantium minima?",
   },
   {
     year: 2022,
-    title: "Internship front-end developer",
+    title: " Internship front-end developer",
     duration: "4 Months",
     details:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi porro ullam non quibusdam autem recusandae delectus tempora asperiores, laudantium minima?",
@@ -34,7 +34,9 @@ const data = [
 const Work = () => {
   return (
     <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl text-bold text-center text-[#001b5e]">Work</h1>
+      <h1 className="text-4xl font-semibold text-center text-[#001b5e]">
+        Work
+      </h1>
       {data.map((item, idx) => (
         <WorkItem
           key={idx}
