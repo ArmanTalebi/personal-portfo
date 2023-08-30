d8cf73e1e9108c673ded8f8efbba97a4d83adcc2
Arman <Arman.talebi2000@gmail.com>
2023-08-30 10:31:16 +0300

    Work and WorkItem added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import SideNav from "./components/SideNav";
 import Main from "./components/Main/Main";
+import Work from "./components/Work";
 
 function App() {
   return (
     <div>
       <SideNav />
       <Main />
+      <Work />
     </div>
   );
 }
