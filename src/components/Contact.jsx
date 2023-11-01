f3f44093ba3cbbda6dae009df2a6e559a33aa2a7
Arman <Arman.talebi2000@gmail.com>
2023-11-01 17:55:13 +0300

    adding form options

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,29 +4,56 @@ const Contact = () => {
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
-      <form action="" method="POST" encType="multipart/form-data">
+      <form
+        action="https://getform.io/f/71ea6c83-ff48-46bd-9f18-cdd336164283"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
           <div>
-            <label>Name</label>
-            <input type="text" name="name" />
+            <label className="uppercase text-sm py-2">Name</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="name"
+            />
           </div>
-          <div>
-            <label>Phone Number</label>
-            <input type="text" name="phone" />
+          <div className="flex flex-col">
+            <label className="uppercase text-sm py-2">Phone</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+              type="text"
+              name="phone"
+            />
           </div>
         </div>
-        <div>
-          <label>Email</label>
-          <input type="text" name="email" />
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2">Email</label>
+          <input
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            type="text"
+            name="email"
+          />
         </div>
-        <div>
-          <label>Subject</label>
-          <input type="text" name="subject" />
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2">Subject</label>
+          <input
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            type="text"
+            name="subject"
+          />
         </div>
-        <div>
-          <label>Message</label>
-          <textarea rows="10" name="message"></textarea>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2">Message</label>
+          <textarea
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            rows="10"
+            name="message"
+          ></textarea>
         </div>
+        <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
+          Send Message
+        </button>
       </form>
     </div>
   );
